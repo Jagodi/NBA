a9746d6a21ef64584c17c5c30fcedf023cd22fb1
Yuriy <3150498@gmail.com>
2020-11-20 04:38:32 +0300

    fix broken commit and add aop exception handler

--- src/main/java/edu/devember/NBA/NbaApplication.java
+++ src/main/java/edu/devember/NBA/NbaApplication.java
@@ -1,4 +1,4 @@
-package edu.devember.NBA.configuration;
+package edu.devember.NBA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
